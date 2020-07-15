11f6a556b9f276126aaa0425ce481cd98daa2dbe
prosscode <rukiapross@foxmail.com>
2020-07-15 19:28:17 +0800

    BIO Demo

--- src/main/java/pross/code/io/bio/SocketServer1.java
+++ src/main/java/pross/code/io/bio/SocketServer1.java
@@ -1,5 +1,7 @@
 package pross.code.io.bio;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -7,7 +9,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * @describe: 服务器端（NIOSocketServer）单个线程处理
+ * @describe: 服务器端（BIOSocketServer）单个线程处理
  * @author: 彭爽 pross.peng
  * @date: 2020/07/05
  */
@@ -19,13 +21,10 @@ public class SocketServer1 {
         OutputStream out = null;
         try {
             serverSocket = new ServerSocket(8888);
-            //Java中非阻塞同步IO模式通过设置此方法实现
-//            serverSocket.setSoTimeout(1000);
             while (true) {
                 System.out.println("启动服务，等待消息...");
                 Socket accept = serverSocket.accept();
-
-                //下面我们收取信息
+                //收取信息
                 in = accept.getInputStream();
                 out = accept.getOutputStream();
                 Integer sourcePort = accept.getPort();
@@ -35,30 +34,39 @@ public class SocketServer1 {
                 int realLen = in.read(contextBytes, 0, maxLen);
                 //读取信息
                 String message = new String(contextBytes, 0, realLen);
-
                 //下面打印信息
                 System.out.println("服务器收到来自于端口：" + sourcePort + "的信息：" + message);
 
-                //下面开始发送信息
-                out.write("回发响应信息！".getBytes());
-                //关闭
-                out.close();
+                //回应信息
+                if(StringUtils.equals(message,"exit")){
+                    out.write("正在关闭服务端！".getBytes());
+                    out.flush();
+                    break;
+                }else{
+                    out.write("回发响应信息！".getBytes());
+                    out.flush();
+                    System.out.println("已回复消息！");
+                }
                 in.close();
-                accept.close();
+                out.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-                try {
-                    if(in != null) {
-                        in.close();
-                    }
-                    if(out != null){
-                        out.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        } finally {
+            // 关闭
+            try {
+                if (in != null) {
+                    in.close();
                 }
+                if (out != null) {
+                    out.close();
+                }
+                if(serverSocket!=null){
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
