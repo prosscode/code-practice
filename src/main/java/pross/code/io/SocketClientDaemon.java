a7c9b824df567ddc805c49b0c251eaec80a5ff11
prosscode <rukiapross@foxmail.com>
2020-08-11 22:06:19 +0800

    kafka,producer demo

--- src/main/java/pross/code/io/SocketClientDaemon.java
+++ src/main/java/pross/code/io/SocketClientDaemon.java
@@ -10,6 +10,13 @@ import java.util.concurrent.CountDownLatch;
  */
 public class SocketClientDaemon {
 
+    /**
+     * 同步io：阻塞/非阻塞/多路复用
+     *  只有上层（包括上层的某种代理机制）系统询问我是否有某个事件发生了，否则我不会主动告诉上层系统事件发生了：
+     * @param args
+     * @throws InterruptedException
+     */
+
     public static void main(String[] args) throws InterruptedException {
         int clientNumber = 20;
         CountDownLatch countDownLatch = new CountDownLatch(clientNumber);
