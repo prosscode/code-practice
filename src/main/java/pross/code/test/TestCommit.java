692a98343c1e0b11197098eb15a1b876009c120e
prosscode <rukiapross@foxmail.com>
2020-08-23 16:50:06 +0800

    test commit

---
+++ src/main/java/pross/code/test/TestCommit.java
@@ -0,0 +1,13 @@
+package pross.code.test;
+
+/**
+ * @describe:
+ * @author: 彭爽 pross.peng
+ * @date: 2020/08/23
+ */
+public class TestCommit {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
